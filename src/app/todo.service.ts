643b8572d5176c27a6db9c659c93f71744612599
Hardi99 <harditabuna@gmail.com>
2024-12-25 15:30:27 +0100

    fix todo.service AND app.config.ts

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -1,13 +1,16 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({
+  providedIn: 'root'
+})
 
 export class TodoService {
+  private http = inject(HttpClient);
   private apiUrl = 'http://localhost:3000/todos';
 
-  constructor(private http: HttpClient) {}
+  /* constructor(private http: HttpClient) {} */
 
   getTodos(): Observable<any[]> {
     return this.http.get<any[]>(this.apiUrl);
